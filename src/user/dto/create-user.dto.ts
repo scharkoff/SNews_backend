7792685767a948d142ee5b8733a7e95b43e456d1
Sharkov Ivan <sharkov.agent@mail.ru>
2023-03-22 17:37:28 +0300

    Create user CRUD
    - Create user entity
    - Create user service logic
    - Create user controller

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,1 +1,17 @@
-export class CreateUserDto {}
+import { IsEmail, Length } from 'class-validator';
+
+export class CreateUserDto {
+  @Length(3, 30)
+  login: string;
+
+  password_hash: string;
+
+  @Length(3, 30)
+  last_name: string;
+
+  @Length(3, 30)
+  first_name: string;
+
+  @IsEmail()
+  email: string;
+}
--- src/user/entities/user.entity.ts
+++ src/user/entities/user.entity.ts
@@ -1,1 +1,22 @@
-export class User {}
+import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class UserEntity {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  login: string;
+
+  @Column()
+  password_hash: string;
+
+  @Column()
+  last_name: string;
+
+  @Column()
+  first_name: string;
+
+  @Column()
+  email: string;
+}
