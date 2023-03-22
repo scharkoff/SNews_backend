a7dffeda75b0015464707872a7a1023255fdd9d8
Sharkov Ivan <sharkov.agent@mail.ru>
2023-03-22 13:09:44 +0300

    Start template

---
+++ src/main.ts
@@ -0,0 +1,10 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  await app.listen(5555);
+  console.log('Server started on port 5555');
+}
+
+bootstrap();
