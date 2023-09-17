96232f4164030546022f94d7540dda60f30aa196
Sharkov Ivan <sharkov.agent@mail.ru>
2023-09-17 22:45:22 +0300

    Enable cors

--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,17 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
   app.useGlobalPipes(new ValidationPipe());
+
+  app.enableCors({
+    origin: 'http://localhost:3000',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
+
   await app.listen(5555);
+
   console.log('Server started on port 5555');
 }
 
--- src/post/post.controller.ts
+++ src/post/post.controller.ts
@@ -44,7 +44,7 @@ export class PostController {
 
   @Get(':id')
   findOne(@Param('id') id: string) {
-    return this.postService.findOne(+id);
+    return this.postService.findOne(+id || 0);
   }
 
   @Patch(':id')
